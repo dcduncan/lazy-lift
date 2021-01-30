d969f16dc719442ae7a7cc09ae8eea292cd7b1bc
Daniel Duncan <dcduncan.code@gmail.com>
2021-01-29 20:55:55 -0800

    Able to serve static app from server

--- src/main/kotlin/main/Main.kt
+++ src/main/kotlin/main/Main.kt
@@ -3,7 +3,12 @@ package main
 import io.javalin.Javalin;
 
 fun main(args: Array<String>) {
-    val app: Javalin = Javalin.create().start(7000)
-    app.get("/") { ctx -> ctx.result("Hello World") }
+    val app = Javalin.create { config ->
+        config
+            .addStaticFiles("/static/build")
+            .addStaticFiles("/")
+            .addSinglePageRoot("/", "/static/build/index.html")
+    }.start(7000)
+
     app.get("/api/routines") { ctx -> ctx.result(arrayListOf("Legs", "Arms", "Abs").toString())}
 }
