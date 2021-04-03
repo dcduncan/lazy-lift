6627fbc58ba242599b1047dccdbc331d93ce92ea
Daniel Duncan <dcduncan.code@gmail.com>
2021-04-02 22:07:27 -0700

    Update Travis command and handle heroku port

--- src/main/kotlin/main/Main.kt
+++ src/main/kotlin/main/Main.kt
@@ -3,11 +3,12 @@ package main
 import io.javalin.Javalin;
 
 fun main(args: Array<String>) {
+    val port = System.getenv("PORT") ?: "8080"
     val app = Javalin.create { config ->
         config
             .addStaticFiles("/static/build")
             .addSinglePageRoot("/", "/static/build/index.html")
-    }.start(8080)
+    }.start(port.toInt())
 
     app.get("/api/routines") { ctx -> ctx.result(arrayListOf("Legs", "Arms", "Abs").toString())}
 }
