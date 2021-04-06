cd41978866c3d8a4fee72f90b309c2688cec4a65
Daniel Duncan <dcduncan.code@gmail.com>
2021-04-06 15:52:00 -0700

    Extracts out more TypeScript

---
+++ src/main/ui/src/main/App.tsx
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import logo from '../logo.svg';
+import './App.css';
+import FetchRoutinesButton from './FetchRoutinesButton'
+
+function App() {
+  const [routines, setRoutines] = useState('');
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/main/App.jsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        {
+            routines === ''
+            ? <FetchRoutinesButton onClick={setRoutines} text="Fetch Routines"/>
+            :
+                <p>
+                    Routines: {routines}
+                    <div></div>
+                </p>
+        }
+      </header>
+    </div>
+  );
+}
+
+export default App;
